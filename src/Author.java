14b778ad01b91601e032c3a8e69d2acc0d30c5a9
Fox-Why-Not <Clover.kos.1@gmail.com>
2024-05-27 18:10:54 +0300

    q

--- src/Author.java
+++ src/Author.java
@@ -12,7 +12,15 @@ public class Author {
     public String toString() {
         return this.name + this.lastName;
     }
+
+
+    public boolean equals(Object o) {
+            if (this == o) return  true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Author author = (Author) o;
+            return Objects.equals(name, author.name) && Objects.equals(lastName, author.lastName);
+    }
     public int hashCode() {
-            return Objects.hash(name,lastName);
+        return Objects.hash(name,lastName);
     }
 }
--- src/Book.java
+++ src/Book.java
@@ -20,6 +20,12 @@ public class Book {
     public String toString() {
         return "Имя " + this.name + ", Author " + this.publicationYear;
     }
+    public boolean equals(Object o) {
+        if (this == o) return  true;
+        if (!(o instanceof Book book)) return false;
+        Author author = (Author) o;
+        return publicationYear == book.publicationYear && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
 
     public int hashCode() {
         return Objects.hash(name, author, publicationYear);
